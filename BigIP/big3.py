636864d4fe779dff2348f7dfec6b94bdbcde9783
dsea <dsea11@live.com>
2016-06-27 15:17:44 -0500

    Initial commit for start of device connection port of bigsuds

--- BigIP/big3.py
+++ BigIP/big3.py
@@ -1,4 +1,25 @@
-import urllib
-import logging
 import ssl
+import rinse
+import urllib
+import ssl
+import os
+import logging
+import http
+import requests
 
+
+from rinse import client
+from http.client import HTTPSConnection
+
+class DEVCONN(object):
+    def __init__(self, hostname, username='admin', password='admin',
+                 debug=False, cachedir=None, verify=False, timeout=90, port=443):
+        self._hostname = hostname
+        self._username = username
+        self._password = password
+        self._port = port
+        self._debug = debug
+        self._cachedir = cachedir
+        self._timeout = timeout
+
+    def
---
+++ Rinse_Dep/RisnseConns.py
@@ -0,0 +1,4 @@
+from rinse import response
+
+class DevConnect(object):
+    def deviceconnection:
